b36b2a87bc4d7debb1b3d9eac46e502eb7a5869d
Csomorka <nagycsomoristvan@gmail.com>
2025-07-21 14:12:45 +0200

    Fix mobile layout by using min-h-[100vh] instead of h-screen

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,7 +13,7 @@ function AppLayout() {
 
   return (
     <>
-      <div className="relative min-h-screen lg:grid lg:h-screen lg:grid-cols-[1.5fr_4fr] lg:grid-rows-[auto_1fr]">
+      <div className="relative min-h-[100vh] lg:grid lg:h-screen lg:grid-cols-[1.5fr_4fr] lg:grid-rows-[auto_1fr]">
         <Header toggleNav={toggleNav} />
         <SideBar />
         <main className="overflow-x-auto bg-stone-100 md:px-4 md:py-4 lg:overflow-x-scroll">
